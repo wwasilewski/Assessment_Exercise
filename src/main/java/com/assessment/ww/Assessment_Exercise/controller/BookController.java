9b18eea58e15c9136d4a808ae1f0a002db448351
wwasilewski <w.wasilewski@gmail.com>
2019-07-12 00:40:52 +0200

    adding BookController with some REST mappings

---
+++ src/main/java/com/assessment/ww/Assessment_Exercise/controller/BookController.java
@@ -0,0 +1,68 @@
+package com.assessment.ww.Assessment_Exercise.controller;
+
+import com.assessment.ww.Assessment_Exercise.model.Book;
+import com.assessment.ww.Assessment_Exercise.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 11.07.2019
+ */
+@RestController
+public class BookController {
+
+    private BookService bookService;
+
+    // injecting bookService via constructor
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // expose /books and get list of all books - GET
+    @GetMapping("/getAllBooks")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    // add mapping for getting specific book - GET
+    @GetMapping("/books/{bookId}")
+    public Book getBookById(@PathVariable long bookId) {
+
+        Book book = bookService.findBookById(bookId);
+
+        // throw exception if null
+        if (book == null) {
+            throw new RuntimeException("Didn't find book with id: " + bookId);
+        }
+        return book;
+    }
+
+    // add mapping for adding book - POST
+    @PostMapping("/addBook")
+    public Book addBook(@RequestBody Book book) {
+
+        // set id to 0 in case they pass id in JSON
+        // to force a save of new item instead od update
+        book.setId(0);
+        bookService.addBook(book);
+        return book;
+    }
+
+    // add mapping for deleting book - DELETE
+    @DeleteMapping("/deleteBook/{bookId}")
+    public String deleteBookById(@PathVariable long bookId) {
+        Book tempBook = bookService.findBookById(bookId);
+
+        // throw exception if null
+        if (tempBook == null) {
+            throw new RuntimeException("There is no book with id: " + bookId);
+        }
+        bookService.deleteBookById(bookId);
+
+        return "Deleted book with id: " + bookId;
+    }
+}
