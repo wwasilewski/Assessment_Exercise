007dcd319e58f048ac3d95d9cee1e9357dd54c7c
wwasilewski <w.wasilewski@gmail.com>
2019-07-11 17:18:45 +0200

    Initial commit

---
+++ src/main/java/com/assessment/ww/Assessment_Exercise/AssessmentExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.ww.Assessment_Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/ww/Assessment_Exercise/AssessmentExerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assessment.ww.Assessment_Exercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssessmentExerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
