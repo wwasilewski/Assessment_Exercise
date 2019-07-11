e3ba6e078f94f3e19e8c90ef933d4ad9d8ab945c
wwasilewski <w.wasilewski@gmail.com>
2019-07-11 19:29:43 +0200

    adding Book class to the model layer

---
+++ src/main/java/com/assessment/ww/Assessment_Exercise/model/Book.java
@@ -0,0 +1,80 @@
+package com.assessment.ww.Assessment_Exercise.model;
+
+import javax.persistence.*;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 11.07.2019
+ */
+@Entity
+@Table(name = "book")
+public class Book {
+
+    // auto-generate id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    // no args constructor is required by hibernate (because of reflection)
+    public Book() {
+    }
+
+    // no need to add id parameter here - it will be generated automatically
+    public Book(String author, String title, String isbn) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+}
