2d552efad268ecdaa9cd55205239daeac731958e
wwasilewski <w.wasilewski@gmail.com>
2019-07-19 21:35:07 +0200

    added Author class

---
+++ src/main/java/com/assessment/ww/Assessment_Exercise/model/Author.java
@@ -0,0 +1,70 @@
+package com.assessment.ww.Assessment_Exercise.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 19.07.2019
+ */
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String forename;
+    private String surname;
+
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String forename, String surname) {
+        this.forename = forename;
+        this.surname = surname;
+    }
+
+    public Author(String forename, String surname, Set<Book> books) {
+        this.forename = forename;
+        this.surname = surname;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
