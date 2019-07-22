62beed3a5fa00a26f75774de0415a88a5bc1ffcf
wwasilewski <w.wasilewski@gmail.com>
2019-07-22 20:17:52 +0200

    added log4j2.xml with logging configuration, excluded logback in pom.xml, added log4j2 Logger in starting class, removed logging setup in application.properties

--- src/main/java/com/assessment/ww/Assessment_Exercise/AssessmentExerciseApplication.java
+++ src/main/java/com/assessment/ww/Assessment_Exercise/AssessmentExerciseApplication.java
@@ -1,12 +1,18 @@
 package com.assessment.ww.Assessment_Exercise;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AssessmentExerciseApplication {
 
+    public static final Logger logger = LogManager.getLogger(AssessmentExerciseApplication.class);
+
     public static void main(String[] args) {
+
         SpringApplication.run(AssessmentExerciseApplication.class, args);
+        logger.info("Starting SB app");
     }
 }
